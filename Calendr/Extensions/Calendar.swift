4c7bb2a793342d6cf8eb1cfc3df3978a42e2a839
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2021-01-01 18:22:12 -0300

    Calendar "in range" extension tests

--- Calendr/Extensions/Calendar.swift
+++ Calendr/Extensions/Calendar.swift
@@ -9,10 +9,21 @@ import Foundation
 
 extension Calendar {
     func isDate(_ date: Date, in range: (start: Date, end: Date), toGranularity component: Calendar.Component) -> Bool {
+
+        let (start, end) = range
+
+        if end < start {
+            return false
+        }
+
         // date >= start
-        let gte = compare(date, to: range.start, toGranularity: component) != .orderedAscending
+        let gte = compare(date, to: start, toGranularity: component) != .orderedAscending
+
+        // fix range ending at 00:00 of the next day
+        let fixedEnd = start == end ? end : self.date(byAdding: .second, value: -1, to: end)!
+
         // date <= end
-        let lte = compare(date, to: range.end.advanced(by: -1), toGranularity: component) != .orderedDescending
+        let lte = compare(date, to: fixedEnd, toGranularity: component) != .orderedDescending
 
         return gte && lte
     }
---
+++ CalendrTests/CalendarExtensionTests.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarExtensionTests.swift
+//  CalendrTests
+//
+//  Created by Paker on 01/01/21.
+//
+
+import XCTest
+@testable import Calendr
+
+class CalendarExtensionTests: XCTestCase {
+
+    func testDateInRange() {
+        let date = make(day: 5)
+
+        let ranges: [(start: Date, end: Date, expected: Bool)] = [
+            (make(day: 2), make(day: 1), false), // invalid range
+            (make(day: 1), make(day: 5), false),
+            (make(day: 6), make(day: 10), false),
+            (make(day: 1), make(day: 5, second: 1), true), // including end
+            (make(day: 5), make(day: 5), true),
+            (make(day: 5), make(day: 6), true),
+            (make(day: 4), make(day: 6), true)
+        ]
+
+        for (start, end, expected) in ranges {
+            let result = Calendar.current.isDate(date, in: (start, end), toGranularity: .day)
+            XCTAssertEqual(expected, result, "\(start) - \(end))")
+        }
+    }
+
+    private func make(day: Int, hour: Int = 0, minute: Int = 0, second: Int = 0) -> Date {
+        return Calendar.current.date(from: .init(day: day, hour: hour, minute: minute, second: second))!
+    }
+}
