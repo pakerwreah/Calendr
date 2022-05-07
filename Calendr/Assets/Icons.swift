a36afee2ab71ab295b7c956f53b149e8fbe63d69
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2022-05-07 22:26:08 +0100

    Revise `pin` and `reset` icons

--- Calendr/Assets/Icons.swift
+++ Calendr/Assets/Icons.swift
@@ -15,14 +15,14 @@ enum Icons {
 
     enum Calendar {
         static let prev = NSImage(systemName: "chevron.backward")
-        static let reset = NSImage(systemName: "arrow.clockwise")
+        static let reset = NSImage(systemName: "circle")
         static let next = NSImage(systemName: "chevron.forward")
         static let picker = NSImage(systemName: "checkmark.square")
         static let reminders = NSImage(systemName: "list.bullet")
         static let calendar = NSImage(systemName: "calendar")
         static let settings = NSImage(systemName: "ellipsis.circle")
-        static let pinned = NSImage(systemName: "lock")
-        static let unpinned = NSImage(systemName: "lock.open")
+        static let pinned = NSImage(systemName: "pin.fill")
+        static let unpinned = NSImage(systemName: "pin")
         static let scalingMinus = NSImage(systemName: "minus.magnifyingglass")
         static let scalingPlus = NSImage(systemName: "plus.magnifyingglass")
     }
