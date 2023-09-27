1363bc9af6089e5227f3b679c5c4fb8b393e3c96
Carlos Enumo <paker_wreah@hotmail.com>
2023-09-27 15:45:15 +0100

    Add new required permissions on macOS 14 (#150)

---
+++ Calendr/Extensions/Error.swift
@@ -0,0 +1,20 @@
+//
+//  Error.swift
+//  Calendr
+//
+//  Created by Paker on 27/09/23.
+//
+
+import Foundation
+
+struct UnexpectedError: LocalizedError {
+
+    let message: String
+
+    var errorDescription: String? { message }
+}
+
+extension Error where Self == UnexpectedError {
+
+    static func unexpected(_ message: String) -> Self { .init(message: message) }
+}
