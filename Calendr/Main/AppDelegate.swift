7c37725202c1b58b65ca72f1bd6f524d13c317ae
Carlos Enumo <paker_wreah@hotmail.com>
2024-06-10 17:35:06 +0100

    Fix desktop click not resigning focus #190

--- Calendr/Main/AppDelegate.swift
+++ Calendr/Main/AppDelegate.swift
@@ -32,30 +32,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             notificationCenter: .default
         )
 
-        setUpKeyboard()
+        setUpEditShortcuts()
+        setUpResignFocus()
     }
-
-    // 🔨 This will not be visible, but it allows us to use basic commands in text fields
-    private func setUpKeyboard() {
-
-        let mainMenu = NSMenu(title: "MainMenu")
-        let menuItem = mainMenu.addItem(withTitle: "", action: nil, keyEquivalent: "")
-
-        let submenu = NSMenu()
-        submenu.addItem(withTitle: "Close Window", action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w")
-        submenu.addItem(withTitle: "Undo", action: #selector(EditMenuActions.undo(_:)), keyEquivalent: "z")
-        submenu.addItem(withTitle: "Redo", action: #selector(EditMenuActions.redo(_:)), keyEquivalent: "Z")
-        submenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
-        submenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
-        submenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
-        submenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
-
-        mainMenu.setSubmenu(submenu, for: menuItem)
-        NSApp.mainMenu = mainMenu
-    }
-}
-
-@objc private protocol EditMenuActions {
-    func redo(_ sender: AnyObject)
-    func undo(_ sender: AnyObject)
 }
