5d01fea2a91fb404aae058f203096a77e86eb3ab
Carlos Enumo <paker_wreah@hotmail.com>
2024-08-18 21:58:17 +0100

    Disable app hang tracking (too much noise)

--- Calendr/Utils/Sentry.swift
+++ Calendr/Utils/Sentry.swift
@@ -15,24 +15,7 @@ func startSentry() -> Span? {
 
 private func configureSentry(_ dsn: String, _ options: Options) {
     options.dsn = dsn
-
-    if ProcessInfo.processInfo.architecture == .x86_64 {
-        options.appHangTimeoutInterval += 1
-    }
-
-    options.beforeSend = { event in
-        if (event.exceptions?.first?.type == "App Hanging") {
-            event.level = .debug
-        }
-        return event
-    }
-}
-
-/// Prevents reporting false hangings for known blocking operations (i.e. context menus)
-func blocking<T>(operation: () -> T) -> T {
-    SentrySDK.pauseAppHangTracking()
-    defer { SentrySDK.resumeAppHangTracking() }
-    return operation()
+    options.enableAppHangTracking = false
 }
 
 extension TransactionContext {
