55c25153d18563b218fc08378294940223d68bcd
Carlos Enumo <paker_wreah@hotmail.com>
2024-08-21 13:20:24 +0100

    Add `system_uptime` to app launch event

--- Calendr/Utils/Sentry.swift
+++ Calendr/Utils/Sentry.swift
@@ -9,8 +9,29 @@ import Sentry
 
 func startSentry() -> Span? {
     guard let dsn = Environment.SENTRY_DSN else { return nil }
+
     SentrySDK.start { configureSentry(dsn, $0) }
-    return SentrySDK.startTransaction(transactionContext: .appLaunch)
+
+    let transaction = SentrySDK.startTransaction(transactionContext: .appLaunch())
+
+    addSystemUptimeInfo(to: transaction)
+
+    return transaction
+}
+
+/**
+ * Investigating why some `app launch` transactions are taking so long.
+ * Trying to see if that's because of high cpu load when user logs in.
+ * Ideally we should get the `login_uptime`, but we can't because of `sandbox`.
+ */
+private func addSystemUptimeInfo(to transaction: Span) {
+    /// This is searchable, but not yet suggested in the filter input.
+    /// i.e. `measurements.system_uptime:<60k` (less than 1min)
+    transaction.setMeasurement(
+        name: "system_uptime",
+        value: NSNumber(value: ProcessInfo.processInfo.systemUptime),
+        unit: MeasurementUnitDuration.second
+    )
 }
 
 private func configureSentry(_ dsn: String, _ options: Options) {
@@ -19,5 +40,5 @@ private func configureSentry(_ dsn: String, _ options: Options) {
 }
 
 extension TransactionContext {
-    static let appLaunch = TransactionContext(name: "app", operation: "launch", sampled: .yes)
+    static func appLaunch() -> Self { .init(name: "app", operation: "launch", sampled: .yes) }
 }
