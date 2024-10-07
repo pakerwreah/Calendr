35246bfbbb124b8e417339ffc3ee1a5449f301ac
Carlos Enumo <paker_wreah@hotmail.com>
2024-10-07 21:32:30 +0100

    Fix opening main popover on mouse down

--- Calendr/Main/MainViewController.swift
+++ Calendr/Main/MainViewController.swift
@@ -844,6 +844,11 @@ private extension NSMenu {
     }
 }
 
+/**
+ We trigger `left` click on mouse `down` and `right` click on mouse `up` on purpose.
+ Mouse down feels faster, but NSMenu.popUp blocks the event chain and causes the button to be stuck in a weird state.
+ It's not about the highlight effect, it really gets stuck and we have to click it twice for it to work again.
+ **/
 private class StatusItemClickHandler {
     let leftClick = PublishSubject<Void>()
     let rightClick = PublishSubject<Void>()
@@ -854,7 +859,7 @@ private class StatusItemClickHandler {
         switch event.type {
         case .leftMouseDown:
             self.leftClick.onNext(())
-        case .rightMouseDown:
+        case .rightMouseUp:
             self.rightClick.onNext(())
         default:
             break
@@ -865,7 +870,7 @@ private class StatusItemClickHandler {
 private extension NSStatusBarButton {
 
     func setUpClickHandler(_ handler: StatusItemClickHandler) {
-        sendAction(on: [.leftMouseUp, .rightMouseUp])
+        sendAction(on: [.leftMouseDown, .rightMouseUp])
         target = handler
         action = #selector(StatusItemClickHandler.action)
     }
