e9a913af5211f3a4cd5289d5fb4adc9b4e6983db
Carlos Enumo <paker_wreah@hotmail.com>
2025-11-05 21:41:34 +0000

    Add border to pending next event to improve visibility (#516)

--- Calendr/Events/Background/EventBackground.swift
+++ Calendr/Events/Background/EventBackground.swift
@@ -18,28 +18,8 @@ extension EventBackground {
     var cgColor: CGColor {
         switch self {
             case .clear: .clear
-            case .pending: pendingBackground
+            case .pending: NSColor.gray.striped(alpha: 0.25).cgColor
             case .color(let color): color.cgColor
         }
     }
 }
-
-private let pendingBackground: CGColor = {
-
-    let stripes = CIFilter.stripesGenerator()
-    stripes.color0 = CIColor(color: NSColor.gray.withAlphaComponent(0.25))!
-    stripes.color1 = .clear
-    stripes.width = 2.5
-    stripes.sharpness = 0
-
-    let rotated = CIFilter.affineClamp()
-    rotated.inputImage = stripes.outputImage!
-    rotated.transform = CGAffineTransform(rotationAngle: -.pi / 4)
-
-    let ciImage = rotated.outputImage!.cropped(to: CGRect(x: 0, y: 0, width: 300, height: 300))
-    let rep = NSCIImageRep(ciImage: ciImage)
-    let nsImage = NSImage(size: rep.size)
-    nsImage.addRepresentation(rep)
-
-    return NSColor(patternImage: nsImage).cgColor
-}()
