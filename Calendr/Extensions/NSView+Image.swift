5a533d6bd58d3204b636c92e8ee38aac57280d84
Carlos Enumo <paker_wreah@hotmail.com>
2025-11-05 00:48:11 +0000

    Fix next event text color for different wallpapers

--- Calendr/Extensions/NSView+Image.swift
+++ Calendr/Extensions/NSView+Image.swift
@@ -8,18 +8,31 @@
 import AppKit
 
 extension NSView {
-    
-    func asImage() -> NSImage? {
+
+    func asImage(appearance: NSAppearance? = nil) -> NSImage? {
         layoutSubtreeIfNeeded()
-        
+
         guard let rep = bitmapImageRepForCachingDisplay(in: bounds) else {
             return nil
         }
-        cacheDisplay(in: bounds, to: rep)
-        
+
+        performWithAppearance(appearance) {
+            cacheDisplay(in: bounds, to: rep)
+        }
+
         let image = NSImage(size: bounds.size)
         image.addRepresentation(rep)
-        
+
         return image
+    }
+
+    private func performWithAppearance(_ appearance: NSAppearance?, task: () -> Void) {
+        guard let appearance else {
+            return task()
+        }
+        let previous = self.appearance
+        self.appearance = appearance
+        task()
+        self.appearance = previous
     }
 }
