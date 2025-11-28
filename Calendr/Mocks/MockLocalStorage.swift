pakerwreah/Calendr

commit 5affd7e47e0e99f8ba506aea2fbd10ce65eaf64d

Create local storage abstraction (#500)

Calendr/Mocks/MockLocalStorage.swift
@@ -1,0 +1,85 @@
+//
+//  MockLocalStorage.swift
+//  Calendr
+//
+//  Created by Paker on 28/10/2025.
+//
+
+import Foundation
+
+#if DEBUG
+
+class InMemoryStorage: LocalStorage {
+    private var storage: [String: Any] = [:]
+    private var registeredDefaults: [String: Any] = [:]
+
+    func reset() {
+        storage.removeAll()
+    }
+
+    func register(defaults registrationDictionary: [String : Any]) {
+        for (key, value) in registrationDictionary {
+            registeredDefaults[key] = value
+        }
+    }
+
+    func set(_ value: Any?, forKey name: String) {
+        storage[name] = value
+    }
+
+    func removeObject(forKey name: String) {
+        storage.removeValue(forKey: name)
+    }
+
+    func object(forKey name: String) -> Any? {
+        storage[name] ?? registeredDefaults[name]
+    }
+
+    func data(forKey name: String) -> Data? {
+        object(forKey: name) as? Data
+    }
+
+    func array(forKey name: String) -> [Any]? {
+        object(forKey: name) as? [Any]
+    }
+
+    func dictionary(forKey name: String) -> [String : Any]? {
+        object(forKey: name) as? [String : Any]
+    }
+
+    func string(forKey name: String) -> String? {
+        object(forKey: name) as? String
+    }
+
+    func stringArray(forKey name: String) -> [String]? {
+        object(forKey: name) as? [String]
+    }
+
+    func bool(forKey name: String) -> Bool {
+        object(forKey: name) as? Bool ?? false
+    }
+
+    func double(forKey name: String) -> Double {
+        (object(forKey: name) as? NSNumber)?.doubleValue ?? 0
+    }
+
+    func integer(forKey name: String) -> Int {
+        (object(forKey: name) as? NSNumber)?.intValue ?? 0
+    }
+}
+
+class MockLocalStorageProvider: LocalStorageProvider {
+
+    private let memory: InMemoryStorage
+
+    required init() {
+        memory = InMemoryStorage()
+        super.init(storage: memory)
+    }
+
+    func reset() {
+        memory.reset()
+    }
+}
+
+#endif

Calendr/Providers/LocalStorageProvider.swift
@@ -1,0 +1,127 @@
+//
+//  LocalStorageProvider.swift
+//  Calendr
+//
+//  Created by Paker on 28/10/2025.
+//
+
+import RxSwift
+
+protocol LocalStorage {
+
+    func register(defaults: [String : Any])
+
+    func set(_ value: Any?, forKey: String)
+
+    func removeObject(forKey: String)
+
+    func object(forKey: String) -> Any?
+
+    func data(forKey: String) -> Data?
+
+    func array(forKey: String) -> [Any]?
+
+    func dictionary(forKey: String) -> [String: Any]?
+
+    func string(forKey: String) -> String?
+
+    func stringArray(forKey: String) -> [String]?
+
+    func bool(forKey: String) -> Bool
+
+    func double(forKey: String) -> Double
+
+    func integer(forKey: String) -> Int
+}
+
+/// This wrapper is necessary because we need an `NSObject` to extend with `@objc dynamic properties`
+/// If we add this constraint directly to the protocol, we have to deal with "existential types" which are absolute nightmares
+class LocalStorageProvider: NSObject, LocalStorage {
+
+    private let storage: LocalStorage
+
+    init(storage: LocalStorage) {
+        self.storage = storage
+    }
+
+    func register(defaults: [String : Any]) {
+        storage.register(defaults: defaults)
+    }
+
+    override func value(forKey name: String) -> Any? {
+        object(forKey: name)
+    }
+
+    func set(_ value: Any?, forKey name: String) {
+        willChangeValue(forKey: name)
+        storage.set(value, forKey: name)
+        didChangeValue(forKey: name)
+    }
+
+    func removeObject(forKey name: String) {
+        willChangeValue(forKey: name)
+        storage.removeObject(forKey: name)
+        didChangeValue(forKey: name)
+    }
+
+    func object(forKey name: String) -> Any? {
+        storage.object(forKey: name)
+    }
+
+    func data(forKey name: String) -> Data? {
+        storage.data(forKey: name)
+    }
+
+    func array(forKey name: String) -> [Any]? {
+        storage.array(forKey: name)
+    }
+
+    func dictionary(forKey name: String) -> [String: Any]? {
+        storage.dictionary(forKey: name)
+    }
+
+    func string(forKey name: String) -> String? {
+        storage.string(forKey: name)
+    }
+
+    func stringArray(forKey name: String) -> [String]? {
+        storage.stringArray(forKey: name)
+    }
+
+    func bool(forKey name: String) -> Bool {
+        storage.bool(forKey: name)
+    }
+
+    func double(forKey name: String) -> Double {
+        storage.double(forKey: name)
+    }
+
+    func integer(forKey name: String) -> Int {
+        storage.integer(forKey: name)
+    }
+}
+
+extension UserDefaults: LocalStorage {}
+
+private let _shared = LocalStorageProvider(
+    storage: {
+        #if DEBUG
+        guard !BuildConfig.isUITesting else {
+            return InMemoryStorage()
+        }
+        #endif
+        return UserDefaults.standard
+    }()
+)
+
+extension LocalStorageProvider {
+
+    static var shared: LocalStorageProvider {
+
+        guard !BuildConfig.isTesting else {
+            fatalError("Use memory storage in tests")
+        }
+
+        return _shared
+    }
+}

Calendr.xcodeproj/project.pbxproj
@@ -56,6 +56,8 @@
 		3438AEEF2C6192A9002BE3E1 /* XCTest.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3438AEED2C6192A9002BE3E1 /* XCTest.swift */; };
 		34427692269B5760004CFE1C /* MockMainViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34427691269B5760004CFE1C /* MockMainViewController.swift */; };
 		3442769A269B5CA4004CFE1C /* UITestCase.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34427699269B5CA4004CFE1C /* UITestCase.swift */; };
+		3443D9722EB0EED200476D4B /* MockLocalStorage.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3443D9712EB0EED200476D4B /* MockLocalStorage.swift */; };
+		3443D9742EB0F01500476D4B /* LocalStorageProvider.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3443D9732EB0F01200476D4B /* LocalStorageProvider.swift */; };
 		3449402E25C348B20020E664 /* GeneralSettingsViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3449402D25C348B20020E664 /* GeneralSettingsViewController.swift */; };
 		3449403225C348C70020E664 /* CalendarPickerViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3449403125C348C70020E664 /* CalendarPickerViewController.swift */; };
 		344A59DB29AEBBBF004F0452 /* EventUtils.swift in Sources */ = {isa = PBXBuildFile; fileRef = 344A59DA29AEBBBF004F0452 /* EventUtils.swift */; };
@@ -136,7 +138,7 @@
 		3492C460269FCF5B009CFAD2 /* Accessibility.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3492C45F269FCF5B009CFAD2 /* Accessibility.swift */; };
 		3492C464269FD64D009CFAD2 /* Accessibility.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3492C45F269FCF5B009CFAD2 /* Accessibility.swift */; };
 		3492C466269FD6CE009CFAD2 /* MainViewTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3492C465269FD6CE009CFAD2 /* MainViewTests.swift */; };
-		34934CD628E69520009635D4 /* Prefs+UserDefaults.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34934CD528E69520009635D4 /* Prefs+UserDefaults.swift */; };
+		34934CD628E69520009635D4 /* LocalStoragePrefs.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34934CD528E69520009635D4 /* LocalStoragePrefs.swift */; };
 		34934CD828E71B0F009635D4 /* NSObject+Rx.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34934CD728E71B0F009635D4 /* NSObject+Rx.swift */; };
 		349355A625BCA8D400957945 /* EventListView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 349355A525BCA8D400957945 /* EventListView.swift */; };
 		349355AA25BCA8DF00957945 /* EventView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 349355A925BCA8DF00957945 /* EventView.swift */; };
@@ -295,6 +297,8 @@
 		34427697269B5CA4004CFE1C /* CalendrUITests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = CalendrUITests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
 		34427699269B5CA4004CFE1C /* UITestCase.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = UITestCase.swift; sourceTree = "<group>"; };
 		3442769B269B5CA4004CFE1C /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
+		3443D9712EB0EED200476D4B /* MockLocalStorage.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MockLocalStorage.swift; sourceTree = "<group>"; };
+		3443D9732EB0F01200476D4B /* LocalStorageProvider.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = LocalStorageProvider.swift; sourceTree = "<group>"; };
 		3449402D25C348B20020E664 /* GeneralSettingsViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = GeneralSettingsViewController.swift; sourceTree = "<group>"; };
 		3449403125C348C70020E664 /* CalendarPickerViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = CalendarPickerViewController.swift; sourceTree = "<group>"; };
 		344A59DA29AEBBBF004F0452 /* EventUtils.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = EventUtils.swift; sourceTree = "<group>"; };
@@ -383,7 +387,7 @@
 		3492C45D269F7966009CFAD2 /* UITestCase+Queries.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "UITestCase+Queries.swift"; sourceTree = "<group>"; };
 		3492C45F269FCF5B009CFAD2 /* Accessibility.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = Accessibility.swift; sourceTree = "<group>"; };
 		3492C465269FD6CE009CFAD2 /* MainViewTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MainViewTests.swift; sourceTree = "<group>"; };
-		34934CD528E69520009635D4 /* Prefs+UserDefaults.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "Prefs+UserDefaults.swift"; sourceTree = "<group>"; };
+		34934CD528E69520009635D4 /* LocalStoragePrefs.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = LocalStoragePrefs.swift; sourceTree = "<group>"; };
 		34934CD728E71B0F009635D4 /* NSObject+Rx.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "NSObject+Rx.swift"; sourceTree = "<group>"; };
 		349355A525BCA8D400957945 /* EventListView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = EventListView.swift; sourceTree = "<group>"; };
 		349355A925BCA8DF00957945 /* EventView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = EventView.swift; sourceTree = "<group>"; };
@@ -527,6 +531,7 @@
 				3421DA112693E70E00056837 /* MockEventSettings.swift */,
 				345CC36D2C8CE13100B0DD1A /* MockGeocodeServiceProvider.swift */,
 				34299F742C72507100A0269B /* MockLocalNotificationProvider.swift */,
+				3443D9712EB0EED200476D4B /* MockLocalStorage.swift */,
 				34299F722C724FA000A0269B /* MockNetworkServiceProvider.swift */,
 				346C5A4B2BC2D87F0007106C /* MockNextEventSettings.swift */,
 				34F4C47C2A6C56AA00397C80 /* MockScreenProvider.swift */,
@@ -813,6 +818,7 @@
 				34FD09D825AE269600AAAAE2 /* DateProvider.swift */,
 				345CC36B2C8CCE5400B0DD1A /* GeocodeServiceProvider.swift */,
 				34E855692C7115A100757B58 /* LocalNotificationProvider.swift */,
+				3443D9732EB0F01200476D4B /* LocalStorageProvider.swift */,
 				34299F702C72289400A0269B /* NetworkServiceProvider.swift */,
 				346E70C82802278E00A785FF /* ScreenProvider.swift */,
 				34C39F6C2CD6A15000933E6D /* SoundPlayer.swift */,
@@ -971,7 +977,7 @@
 				34B5A09C25B118EC00F7F7ED /* CalendarPickerViewModel.swift */,
 				3449402D25C348B20020E664 /* GeneralSettingsViewController.swift */,
 				34D88DC72C4C66F80047D636 /* KeyboardViewController.swift */,
-				34934CD528E69520009635D4 /* Prefs+UserDefaults.swift */,
+				34934CD528E69520009635D4 /* LocalStoragePrefs.swift */,
 				34D88DC92C4C687B0047D636 /* SettingsUI.swift */,
 				34B5A09225B0CE6F00F7F7ED /* SettingsViewController.swift */,
 				348B8D0325B2925100E518FE /* SettingsViewModel.swift */,
@@ -1284,6 +1290,7 @@
 				34299F712C72289400A0269B /* NetworkServiceProvider.swift in Sources */,
 				34A0E12D2CDE9609005BF256 /* MockSoundPlayer.swift in Sources */,
 				346C5A4C2BC2D87F0007106C /* MockNextEventSettings.swift in Sources */,
+				3443D9742EB0F01500476D4B /* LocalStorageProvider.swift in Sources */,
 				349868E62C8D4E5E00DBFE8D /* HorizontalScrollView.swift in Sources */,
 				348E704B25C61B8D00B3B160 /* Radio.swift in Sources */,
 				347D0FF825954975002451EC /* CalendarCellViewModel.swift in Sources */,
@@ -1300,6 +1307,7 @@
 				347C04A32D136BF800F67E7C /* SearchSuggestionView.swift in Sources */,
 				34299F732C724FA000A0269B /* MockNetworkServiceProvider.swift in Sources */,
 				34B99FBB2D2C8605003D4EA4 /* EventBackground.swift in Sources */,
+				3443D9722EB0EED200476D4B /* MockLocalStorage.swift in Sources */,
 				3468E653284BD44600B21EC8 /* EventBarStyle.swift in Sources */,
 				34F128E12597B9C9007DF31C /* MainViewController.swift in Sources */,
 				342AEE322DFF069E008CBBE0 /* CalendarViewMode.swift in Sources */,
@@ -1310,7 +1318,7 @@
 				34D55FE325F06669007F5C81 /* Sequence.swift in Sources */,
 				347D0FED2595467A002451EC /* CalendarCellView.swift in Sources */,
 				3421DA212693F12400056837 /* Calendar+Factory.swift in Sources */,
-				34934CD628E69520009635D4 /* Prefs+UserDefaults.swift in Sources */,
+				34934CD628E69520009635D4 /* LocalStoragePrefs.swift in Sources */,
 				34FD09D925AE269600AAAAE2 /* DateProvider.swift in Sources */,
 				342AEE302DFF0686008CBBE0 /* PopoverMaterial.swift in Sources */,
 				34D25E4A292F9E2100557E70 /* ImageButton.swift in Sources */,

Calendr/Components/Scaling.swift
@@ -9,7 +9,7 @@ import Foundation
 import RxSwift
 
 enum Scaling {
-    static let observable = UserDefaults.standard.rx.observe(\.textScaling).share(replay: 1)
+    static let observable = LocalStorageProvider.shared.rx.observe(\.textScaling).share(replay: 1)
 
     static var current: Double {
         var value: Double = 1

Calendr/Events/EventDetails/EventDetailsViewModel.swift
@@ -81,7 +81,7 @@ class EventDetailsViewModel {
         geocoder: GeocodeServiceProviding,
         weatherService: WeatherServiceProviding,
         workspace: WorkspaceServiceProviding,
-        userDefaults: UserDefaults,
+        localStorage: LocalStorageProvider,
         settings: EventSettings,
         isShowingObserver: AnyObserver<Bool>,
         isInProgress: Observable<Bool>,
@@ -206,7 +206,7 @@ class EventDetailsViewModel {
             else {
                 return false
             }
-            if let pattern = userDefaults.showMapBlacklistRegex {
+            if let pattern = localStorage.showMapBlacklistRegex {
                 do {
                     return try Regex(pattern).wholeMatch(in: location) == nil
                 } catch {
@@ -291,7 +291,7 @@ class EventDetailsViewModel {
             return $0.name < $1.name // Otherwise, sort by name
         }
 
-        selectedBrowserIndex = userDefaults.rx.observe(\.defaultBrowserPerCalendar)
+        selectedBrowserIndex = localStorage.rx.observe(\.defaultBrowserPerCalendar)
             .map {
                 let url = if let path = $0[event.calendar.id], let pathUrl = URL(string: path) {
                     pathUrl
@@ -301,9 +301,9 @@ class EventDetailsViewModel {
                 return browserOptions.firstIndex { $0.url == url } ?? 0
             }
 
-        selectedBrowserObserver = userDefaults.rx.observer(for: \.defaultBrowserPerCalendar)
+        selectedBrowserObserver = localStorage.rx.observer(for: \.defaultBrowserPerCalendar)
             .mapObserver { index in
-                var mapping = userDefaults.defaultBrowserPerCalendar
+                var mapping = localStorage.defaultBrowserPerCalendar
                 if index > 0 {
                     mapping[event.calendar.id] = browserOptions[index].url.absoluteString
                 } else {

Calendr/Events/EventList/EventListViewModel.swift
@@ -39,7 +39,7 @@ class EventListViewModel {
     private let geocoder: GeocodeServiceProviding
     private let weatherService: WeatherServiceProviding
     private let workspace: WorkspaceServiceProviding
-    private let userDefaults: UserDefaults
+    private let localStorage: LocalStorageProvider
     private let settings: EventListSettings
     private let scheduler: SchedulerType
     private let refreshScheduler: SchedulerType
@@ -76,7 +76,7 @@ class EventListViewModel {
         geocoder: GeocodeServiceProviding,
         weatherService: WeatherServiceProviding,
         workspace: WorkspaceServiceProviding,
-        userDefaults: UserDefaults,
+        localStorage: LocalStorageProvider,
         settings: EventListSettings,
         scheduler: SchedulerType,
         refreshScheduler: SchedulerType,
@@ -88,7 +88,7 @@ class EventListViewModel {
         self.geocoder = geocoder
         self.weatherService = weatherService
         self.workspace = workspace
-        self.userDefaults = userDefaults
+        self.localStorage = localStorage
         self.settings = settings
         self.scheduler = scheduler
         self.refreshScheduler = refreshScheduler
@@ -228,7 +228,7 @@ class EventListViewModel {
             geocoder: geocoder,
             weatherService: weatherService,
             workspace: workspace,
-            userDefaults: userDefaults,
+            localStorage: localStorage,
             settings: settings,
             isShowingDetailsModal: isShowingDetailsModal.asObserver(),
             isTodaySelected: isTodaySelected,

Calendr/Events/EventList/EventViewModel.swift
@@ -46,7 +46,7 @@ class EventViewModel {
     private let weatherService: WeatherServiceProviding
     private let settings: EventSettings
     private let workspace: WorkspaceServiceProviding
-    private let userDefaults: UserDefaults
+    private let localStorage: LocalStorageProvider
 
     private let disposeBag = DisposeBag()
 
@@ -57,7 +57,7 @@ class EventViewModel {
         geocoder: GeocodeServiceProviding,
         weatherService: WeatherServiceProviding,
         workspace: WorkspaceServiceProviding,
-        userDefaults: UserDefaults,
+        localStorage: LocalStorageProvider,
         settings: EventSettings,
         isShowingDetailsModal: AnyObserver<Bool>,
         isTodaySelected: Bool,
@@ -69,7 +69,7 @@ class EventViewModel {
         self.dateProvider = dateProvider
         self.calendarService = calendarService
         self.workspace = workspace
-        self.userDefaults = userDefaults
+        self.localStorage = localStorage
         self.geocoder = geocoder
         self.weatherService = weatherService
         self.isShowingDetailsModal = isShowingDetailsModal
@@ -301,7 +301,7 @@ class EventViewModel {
             geocoder: geocoder,
             weatherService: weatherService,
             workspace: workspace,
-            userDefaults: userDefaults,
+            localStorage: localStorage,
             settings: settings,
             isShowingObserver: isShowingDetailsModal,
             isInProgress: isInProgress,

Calendr/Main/AppDelegate.swift
@@ -28,15 +28,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         defer { appLaunch?.finish() }
         #endif
 
-        let userDefaults = UserDefaults.standard
+        let localStorage = LocalStorageProvider.shared
         let notificationCenter = NotificationCenter.default
         let fileManager = FileManager.default
 
-        registerDefaultPrefs(in: userDefaults)
+        registerDefaultPrefs(in: localStorage)
 
-        let dateProvider = DateProvider(notificationCenter: notificationCenter, userDefaults: userDefaults)
+        let dateProvider = DateProvider(notificationCenter: notificationCenter, localStorage: localStorage)
         let calendarAppProvider = CalendarAppProvider(dateProvider: dateProvider, appleScriptRunner: AppleScriptRunner())
-        let workspace = Workspace(userDefaults: userDefaults, dateProvider: dateProvider, calendarAppProvider: calendarAppProvider)
+        let workspace = Workspace(localStorage: localStorage, dateProvider: dateProvider, calendarAppProvider: calendarAppProvider)
         let notificationProvider = LocalNotificationProvider()
 
         viewController = MainViewController(
@@ -46,7 +46,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             calendarService: CalendarServiceProvider(
                 dateProvider: dateProvider,
                 workspace: workspace,
-                userDefaults: userDefaults,
+                localStorage: localStorage,
                 notificationCenter: notificationCenter
             ),
             geocoder: GeocodeServiceProvider(),
@@ -55,7 +55,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             screenProvider: ScreenProvider(notificationCenter: notificationCenter),
             notificationProvider: notificationProvider,
             networkProvider: NetworkServiceProvider(),
-            userDefaults: userDefaults,
+            localStorage: localStorage,
             notificationCenter: notificationCenter,
             fileManager: fileManager
         )

commit 6379dc25db626f64d1368c819a107baba29d8e34

Fix next event position not persisted (again)
Regression #486 caused by #500

Calendr/Mocks/MockLocalStorage.swift
@@ -9,7 +9,7 @@ import Foundation
 
 #if DEBUG
 
-class InMemoryStorage: LocalStorage {
+class InMemoryStorage: NSObject, LocalStorage {
     private var storage: [String: Any] = [:]
     private var registeredDefaults: [String: Any] = [:]
 
@@ -23,12 +23,20 @@ class InMemoryStorage: LocalStorage {
         }
     }
 
+    override func value(forKey name: String) -> Any? {
+        object(forKey: name)
+    }
+
     func set(_ value: Any?, forKey name: String) {
+        willChangeValue(forKey: name)
         storage[name] = value
+        didChangeValue(forKey: name)
     }
 
     func removeObject(forKey name: String) {
+        willChangeValue(forKey: name)
         storage.removeValue(forKey: name)
+        didChangeValue(forKey: name)
     }
 
     func object(forKey name: String) -> Any? {

Calendr/Providers/LocalStorageProvider.swift
@@ -6,8 +6,9 @@
 //
 
 import RxSwift
+import RxCocoa
 
-protocol LocalStorage {
+protocol LocalStorage: NSObject {
 
     func register(defaults: [String : Any])
 
@@ -38,7 +39,7 @@ protocol LocalStorage {
 /// If we add this constraint directly to the protocol, we have to deal with "existential types" which are absolute nightmares
 class LocalStorageProvider: NSObject, LocalStorage {
 
-    private let storage: LocalStorage
+    fileprivate let storage: LocalStorage
 
     init(storage: LocalStorage) {
         self.storage = storage
@@ -128,5 +129,13 @@ extension LocalStorageProvider {
     func withDefaults() -> Self {
         registerDefaultPrefs(in: self)
         return self
+    }
+}
+
+extension Reactive where Base: LocalStorageProvider {
+
+    func observe<Element: KVORepresentable>(_ type: Element.Type, _ keyPath: String, options: KeyValueObservingOptions = [.new, .initial]) -> Observable<Element?> {
+
+        (base.storage as NSObject).rx.observe(type, keyPath, options: options)
     }
 }