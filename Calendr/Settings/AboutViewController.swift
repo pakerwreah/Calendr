ee5c2c6681ee3648fbc2f5cef451d115211c1749
Carlos Enumo <paker_wreah@hotmail.com>
2026-05-22 11:06:19 +0100

    Prevent update error alert from background

--- Calendr/Settings/AboutViewController.swift
+++ Calendr/Settings/AboutViewController.swift
@@ -86,13 +86,14 @@ class AboutViewController: NSViewController, SettingsUI {
 
     private func setUpBindings() {
 
-        autoUpdater.error.observe(on: MainScheduler.instance).bind { error in
+        autoUpdater.error.observe(on: MainScheduler.instance).bind { [weak self] error in
+            guard let window = self?.view.window else { return }
             let alert = NSAlert()
             alert.alertStyle = .warning
             alert.icon = NSImage(named: NSImage.cautionName)
             alert.messageText = error.title
             alert.informativeText = error.message
-            alert.runModal()
+            alert.beginSheetModal(for: window)
         }
         .disposed(by: disposeBag)
 
