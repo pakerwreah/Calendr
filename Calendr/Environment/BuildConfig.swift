5f663b577ac7ac5d95200fe8b71852d337c7d673
Carlos Enumo <paker_wreah@hotmail.com>
2026-05-24 22:06:58 +0100

    Clear caches to avoid silent termination

--- Calendr/Environment/BuildConfig.swift
+++ Calendr/Environment/BuildConfig.swift
@@ -15,6 +15,7 @@ enum BuildConfig {
     static let date = String(cString: BUILD_DATE)
     static let time = String(cString: BUILD_TIME)
     static let appVersion = "v\(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String)"
+    static let isSandboxed = ProcessInfo.processInfo.environment.keys.contains("APP_SANDBOX_CONTAINER_ID")
     static let isPreview = ProcessInfo.processInfo.environment.keys.contains("XCODE_RUNNING_FOR_PREVIEWS")
     static let isTesting = NSClassFromString("XCTestCase") != nil
 
---
+++ Calendr/Main/AppCacheCleaner.swift
@@ -0,0 +1,44 @@
+//
+//  AppCacheCleaner.swift
+//  Calendr
+//
+//  Created by Paker on 24/05/2026.
+//
+
+import AppKit
+import Sentry
+
+/// Prevent macOS from killing the app under heavy load to purge the caches (0xBADDD15C)
+class AppCacheCleaner {
+    private let fileManager = FileManager.default
+    private var task: Task<Void, Error>?
+
+    func schedule() {
+        task?.cancel()
+        task = Task { @MainActor in
+            try await Task.sleep(for: .seconds(5))
+            if NSApp.popovers.isEmpty {
+                delete()
+            }
+        }
+    }
+
+    func cancel() {
+        task?.cancel()
+    }
+
+    func delete() {
+        guard
+            BuildConfig.isSandboxed, // just to be safe
+            let cachesURL = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first,
+            fileManager.isDeletableFile(atPath: cachesURL.absoluteString)
+        else { return }
+
+        do {
+            try fileManager.removeItem(at: cachesURL)
+            try fileManager.createDirectory(at: cachesURL, withIntermediateDirectories: true)
+        } catch {
+            SentrySDK.capture(error: error)
+        }
+    }
+}
