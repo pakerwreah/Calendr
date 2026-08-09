a3534d3e7bad17c6a3299794383538f6fc4c71fe
PatrikFonos1 <118204953+PatrikFonos1@users.noreply.github.com>
2026-08-09 20:19:11 +0200

    Add global shortcut for creating events (#735)

--- Calendr/Main/MainViewController.swift
+++ Calendr/Main/MainViewController.swift
@@ -548,9 +548,14 @@ class MainViewController: NSViewController {
 
         let dateComponents = sender?.representedObject as? DateComponents ?? .init()
 
+        openEventEditor(at: mainViewModel.currentSelectedDate, adding: dateComponents)
+    }
+
+    private func openEventEditor(at date: Date, adding dateComponents: DateComponents = .init()) {
+
         let viewModel = EventEditorViewModel(
             startDate: .withCurrentTime(
-                at: mainViewModel.currentSelectedDate,
+                at: date,
                 adding: dateComponents,
                 using: dateProvider
             ),
@@ -877,6 +882,11 @@ class MainViewController: NSViewController {
             self?.closeModals {
                 self?.mainStatusItemClickHandler.leftClick.onNext(())
             }
+        }
+
+        KeyboardShortcuts.onKeyUp(for: .newEvent) { [weak self] in
+            guard let self else { return }
+            openEventEditor(at: dateProvider.now)
         }
 
         KeyboardShortcuts.onKeyUp(for: .showNextEventPopover) { [weak self] in
