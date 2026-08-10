46688eea3065284d231ef93af486419c45220e29
EhsanAzish80 <ehzish@3nsofts.com>
2026-08-10 14:36:34 +0300

    Let shortcut content define scroll extent

--- Calendr/Settings/KeyboardViewController.swift
+++ Calendr/Settings/KeyboardViewController.swift
@@ -46,10 +46,6 @@ class KeyboardViewController: NSViewController, SettingsUI {
         scrollView.contentView.top(equalTo: contentStackView)
         scrollView.contentView.leading(equalTo: contentStackView)
         scrollView.contentView.trailing(equalTo: contentStackView)
-        scrollView.contentView.height(
-            equalTo: contentStackView,
-            priority: .dragThatCanResizeWindow
-        )
 
         let stackView = NSStackView(views: [shortcutsControl, scrollView])
         .with(spacing: Constants.contentSpacing)
