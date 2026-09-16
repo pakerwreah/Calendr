756a5758918725e93a8e306da262104ed53a3146
Carlos Enumo <paker_wreah@hotmail.com>
2026-09-16 14:28:12 +0100

    Fix event list scroll bar style (#799)
    - Prevent macOS from overriding the event list scroll bar style when
    it's changed to "Always" in System Settings.
    - Make the scroll bar thinner and move it slightly to the right so it
    clears the event list trailing components.

--- Calendr/Main/MainViewController.swift
+++ Calendr/Main/MainViewController.swift
@@ -1003,13 +1003,29 @@ class MainViewController: NSViewController {
         EventListSummaryView(summary: eventListViewModel.summary, showSummary: settingsViewModel.showEventListSummary)
     }
 
+    private class EventListScrollView: NSScrollView {
+
+        override var scrollerStyle: NSScroller.Style {
+            set { super.scrollerStyle = .overlay }
+            get { .overlay }
+        }
+
+        override var verticalScroller: NSScroller? {
+            didSet {
+                verticalScroller?.controlSize = .mini
+            }
+        }
+    }
+
     private func makeEventListScroll() -> NSScrollView {
 
-        let scrollView = NSScrollView()
+        let scrollView = EventListScrollView()
 
         scrollView.drawsBackground = false
         scrollView.documentView = eventListView
         scrollView.scrollerStyle = .overlay
+        scrollView.hasVerticalScroller = true
+        scrollView.scrollerInsets.right = -2
 
         scrollView.contentView.edges(equalTo: scrollView)
         scrollView.contentView.edges(equalTo: eventListView).bottom.priority = .dragThatCanResizeWindow
